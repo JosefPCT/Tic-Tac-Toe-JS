8331738bae230d3853b0286c26cd77e639cc4adc
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-04-14 11:42:18 +0800

    feat: created gameboard iife and the logic to create the tictactoe board, a showBoard function

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("Tic tac toe game...initializing...");
+
+// Gameboard IIFE
+// IIFE -> creates a module and immediately execute it, so it can be used immediately
+const gameboard = (function (){
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    //Creates the board of tictactoe, by creating empty rows, then pushing empty items as columns
+    for(let i = 0; i < rows; i++){
+        board[i] = [];
+        for(let j = 0; j < columns; j++){
+            board[i].push("");
+        }
+    }
+
+    const showBoard = () => board;
+
+    const changeBoard = function(player, choice){
+        console.log("showing changeboard method");
+    };
+    return {showBoard, changeBoard};
+})();
